ca2d097076454fe0dcc6ce3be2961978950a1672
Andrey Glauzer <nglauzer@gmail.com>
2017-10-18 09:00:11 -0200

    05

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="source"
 
 function doCompile {
-  ./compile.sh
+  ./cibuild.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
