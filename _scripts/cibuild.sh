3416d3af69107542a804c052203b68dcb160707f
Andrey Glauzer <nglauzer@gmail.com>
2017-10-18 13:01:05 -0200

    Teste 08

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-cd ..
+# skip if build is triggered by pull request
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
 
-jekyll build --config _config.yml
+# enable error reporting to the console
+set -e
+
+# cleanup "_site"
+rm -rf _site
+mkdir _site
+
+# clone remote repo to "_site"
+git clone https://${GH_TOKEN}@github.com/ayastreb/11route.git --branch gh-pages _site
+
+# build with Jekyll into "_site"
+bundle exec jekyll build
+
+# push
+cd _site
+git config user.email "nglauzer@gmail.com"
+git config user.name "Andrey Glauzer"
+git add --all
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --force origin gh-pages
