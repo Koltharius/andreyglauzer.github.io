8bd95202e7631f459a1b9eef7417c754005be89a
Andrey Glauzer <nglauzer@gmail.com>
2017-10-18 13:31:39 -0200

    Teste 13

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -14,15 +14,15 @@ rm -rf _site
 mkdir _site
 
 # clone remote repo to "_site"
-git clone https://${GH_TOKEN}@github.com/andreyglauzer/andreyglauzer.github.io.git --branch source _site
+#git clone https://${GH_TOKEN}@github.com/andreyglauzer/andreyglauzer.github.io.git --branch master _site
 
 # build with Jekyll into "_site"
 bundle exec jekyll build
 
 # push
-cd _site
-git config user.email "nglauzer@gmail.com"
-git config user.name "Andrey Glauzer"
-git add --all
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --force origin source
+#cd _site
+#git config user.email "nglauzer@gmail.com"
+#git config user.name "Andrey Glauzer"
+#git add -A
+#git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+#git push --force origin master
