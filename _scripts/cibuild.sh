dd5f2ee434a08cd273e4f7c86e644b3316ee226b
Andrey Glauzer <nglauzer@gmail.com>
2017-10-18 13:27:02 -0200

    Teste 13

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-$source = "source"
-
 # skip if build is triggered by pull request
 if [ $TRAVIS_PULL_REQUEST == "true" ]; then
   echo "this is PR, exiting"
@@ -16,7 +14,7 @@ rm -rf _site
 mkdir _site
 
 # clone remote repo to "_site"
-git clone https://${GH_TOKEN}@github.com/andreyglauzer/andreyglauzer.github.io.git --branch $source _site
+git clone https://${GH_TOKEN}@github.com/andreyglauzer/andreyglauzer.github.io.git --branch source _site
 
 # build with Jekyll into "_site"
 bundle exec jekyll build
@@ -27,4 +25,4 @@ git config user.email "nglauzer@gmail.com"
 git config user.name "Andrey Glauzer"
 git add --all
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --force origin $source
+git push --force origin source
